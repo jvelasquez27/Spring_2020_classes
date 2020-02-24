1de85a50d1d8d62ef2b1c548a0216061389e0b0c
jvelasquez27 <36054246+jvelasquez27@users.noreply.github.com>
2020-02-23 22:16:44 -0600

    Add files via upload

--- ECE_407/Homework/HW3/Hw3.m
+++ ECE_407/Homework/HW3/Hw3.m
@@ -1,9 +1,7 @@
 clear all; clc; close all;
-
+%
 x = [2 4 0 7 1 2 0 3 2 1 5 4 3]; %sample set
-
 X = [-3 -2 -1 0 1 2 3 4 5 6 7 8 9];
-
 
 xMu = mean(x);
 xVar = var(x);
@@ -13,4 +11,17 @@ for i=1:size(X,2)
     pdf(i) = mvnpdf(X(1,i),xMu,xVar);
 end
 
-stem(-3:9,pdf);title('Normal Distribution')
+% figure(1)
+% stem(-3:9,pdf);title('Normal Distribution');
+
+%============================================
+
+V = [1 2 2 3 1.5 2 1 1 2;2 3 4 4 5 5 5 4 2];
+
+vMu1 = mean(V(1,:));
+vMu2 = mean(V(2,:));
+
+mu = [vMu1;vMu2];
+vCov = cov(V);
+
+pdf1 = mvnpdf
