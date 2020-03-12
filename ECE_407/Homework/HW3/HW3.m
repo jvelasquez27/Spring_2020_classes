7cb2904c37979c11dc4f5dc8c19637ab07d4e7f5
jvelasquez27 <36054246+jvelasquez27@users.noreply.github.com>
2020-03-11 23:09:19 -0500

    Update HW3.m

--- ECE_407/Homework/HW3/HW3.m
+++ ECE_407/Homework/HW3/HW3.m
@@ -1,5 +1,5 @@
 clear all; clc; close all;
-%
+%===============Problem 3===================
 x = [2 4 0 7 1 2 0 3 2 1 5 4 3]; %sample set
 X = [-3 -2 -1 0 1 2 3 4 5 6 7 8 9];
 
@@ -11,30 +11,32 @@ for i=1:size(X,2)
     pdf(i) = mvnpdf(X(1,i),xMu,xVar);
 end
 
-% figure(1)
-% stem(-3:9,pdf);title('Normal Distribution');
+figure(1)
+scatter(-3:9,pdf,'filled');title('Normal Distribution');
 
-%============================================
+%=============Problem 4===============================
 
 V = [1 2 2 3 1.5 2 1 1 2;2 3 4 4 5 5 5 4 2];
 x1 = [1 2 2 3 1.5 2 1 1 2];
 x2 = [2 3 4 4 5 5 5 4 2];
+
 vMu1 = mean(V(1,:));
 vMu2 = mean(V(2,:));
-mu = [vMu1;vMu2];
+mu = [vMu1 vMu2];
 vCov = cov(V(1,:),V(2,:));
-% sigma = vCov + .1*eye(9);% fixing positive semi-definite problem
-pdf2 = zeros(2,size(V,2));
 
-for i=1:2
-    for j=1:size(V,2)
-        pdf2(i,j) = mvnpdf(V(:,j),mu(i),vCov);
-    end
-end
+x1 = -2:.2:8;
+x2 = -2:.2:8;
+[X1,X2] = meshgrid(x1,x2);
+X = [X1(:) X2(:)];
 
-% for j=1:size(V,2)
-%         pdf2(:,j) = mvnpdf(V(:,j),mu,vCov);
-% end
-% y = mvnpdf(X,mu,sigma);
-% y = reshape(pdf2,length(x2),length(x1));
+pdf2 = mvnpdf(X,mu,vCov);
+pdf2 = reshape(pdf2,length(x1),length(x2));
 
+figure(2)
+surf(x1,x2,pdf2)
+caxis([min(pdf2(:))-0.5*range(pdf2(:)),max(pdf2(:))]);
+
+xlabel('x1')
+ylabel('x2')
+zlabel('Probability Density')
